83879ef42f34c75200019ffa5236aa06e3b89351
imakyo97 <kyoheishogun.1997@gmail.com>
2024-04-17 16:53:55 +0900

    カレンダーUIを作成

--- app/src/main/java/com/example/calendarapp/MainActivity.kt
+++ app/src/main/java/com/example/calendarapp/MainActivity.kt
@@ -1,20 +1,60 @@
 package com.example.calendarapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.calendarapp.databinding.ActivityMainBinding
+import com.example.calendarapp.databinding.DayItemBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    private val days = (1..31).toList().toIntArray()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val activityMainView = activityMainBinding.root
+        setContentView(activityMainView)
+
+        val rvCalendar = activityMainBinding.rvCalendar
+        val numberOfColumns = 7
+        val layout = GridLayoutManager(applicationContext, numberOfColumns)
+        rvCalendar.layoutManager = layout
+        val adapter = RecyclerListAdapter(days)
+        rvCalendar.adapter = adapter
+    }
+
+    private inner class RecyclerListViewHolder(binding: DayItemBinding): RecyclerView.ViewHolder(binding.root) {
+        var tvDay: TextView
+
+        init {
+            // 日付Viewの高さを指定
+            itemView.layoutParams.height = activityMainBinding.rvCalendar.height / 5
+            tvDay = binding.tvDay
+        }
+    }
+
+    private inner class RecyclerListAdapter(private val listData: IntArray): RecyclerView.Adapter<RecyclerListViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerListViewHolder {
+            val binding = DayItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return RecyclerListViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: RecyclerListViewHolder, position: Int) {
+            val day = listData[position].toString()
+            holder.tvDay.text = day
+        }
+
+        override fun getItemCount(): Int {
+            return listData.size
         }
     }
 }
